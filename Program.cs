b24314f6f24fbdbb49cddefbacbe3e3d6f5378b4
Jose Villa <j0542420@amarillocollege.com>
2025-01-29 21:14:44 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk3Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarations
+            string myString = "";
+            
+            //Enter a string
+            Console.WriteLine("Enter a string: ");
+            //
+            myString = Console.ReadLine();
+
+            //
+            Console.WriteLine(myString.IndexOf("a"));
+
+
+
+
+
+
+
+
+        }
+    }
+}
